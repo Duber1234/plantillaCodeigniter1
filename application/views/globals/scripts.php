021e0fc038286ca66ceb1cea23d239d4a8302a77
Duber1234 <dfpesca@misena.edu.co>
2020-08-06 17:03:18 -0500

    inicial

---
+++ application/views/globals/scripts.php
@@ -0,0 +1,6 @@
+
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/bootstrap/css/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/bootstrap/css/bootstrap-reboot.css">
+<script type="text/javascript" src="<?=base_url()?>assets/js/jquery.js"></script>
+<script type="text/javascript" src="<?=base_url()?>assets/bootstrap/js/bootstrap.js"></script>
